578e85596c014ab9c9dcd652adff8f185a651b7d
Deepak Guptha <vzan2012@gmail.com>
2021-02-07 20:50:14 +0530

    feat: Updation of Shopping List and Recipe Components

--- src/app/recipes/recipe.model.ts
+++ src/app/recipes/recipe.model.ts
@@ -1,11 +1,16 @@
 export class Recipe {
-    public name: string;
-    public description: string;
-    public imagePath: string;
+    // Older way 
+    // public name: string;
+    // public description: string;
+    // public imagePath: string;
 
-    constructor(name: string, desc: string, imagePath: string) {
-        this.name = name;
-        this.description = desc;
-        this.imagePath = imagePath
+    // constructor(name: string, desc: string, imagePath: string) {
+    //     this.name = name;
+    //     this.description = desc;
+    //     this.imagePath = imagePath
+    // }
+
+    // Another way 
+    constructor(public name: string, public description: string, public imagePath: string) {
     }
 }
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,11 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Ingredient } from './../shared/ingredient.model'
+
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.scss']
 })
 export class ShoppingListComponent implements OnInit {
+
+  ingredients: Ingredient[] = [
+    new Ingredient('Garlic', 5),
+    new Ingredient('Pepper', 5)
+  ]
 
   constructor() { }
 
