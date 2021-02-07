b4b644950b95f13708a061fcc7aa2bcdbb693bf8
Deepak Guptha <vzan2012@gmail.com>
2021-02-07 19:12:47 +0530

    feat: Updation of Recipe List Components

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,11 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from './../recipe.model'
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.scss']
 })
 export class RecipeListComponent implements OnInit {
+
+
+  recipes: Recipe[] = [
+    new Recipe('Test Recipe 1', 'Test Desc about the recipe1', 'https://via.placeholder.com/728x90.png?text=Test+Recipe+1'),
+    new Recipe('Test Recipe 2', 'Test Desc about the recipe2', 'https://via.placeholder.com/728x90.png?text=Test+Recipe+2'),
+    new Recipe('Test Recipe 3', 'Test Desc about the recipe3', 'https://via.placeholder.com/728x90.png?text=Test+Recipe+3'),
+  ];
 
   constructor() { }
 
