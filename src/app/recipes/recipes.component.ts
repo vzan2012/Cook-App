9ab59425c9d5b6fc176a5aeda02af84fdd2c24f9
Deepak Guptha <vzan2012@gmail.com>
2021-08-01 21:03:54 +0530

    feat: Update image preview and recipe services

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,11 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { RecipeService } from './recipe.service';
+// import { RecipeService } from './recipe.service';
 
 @Component({
   selector: 'app-recipes',
   templateUrl: './recipes.component.html',
   styleUrls: ['./recipes.component.scss'],
-  providers: [RecipeService]
+  // providers: [RecipeService]
 })
 export class RecipesComponent implements OnInit {
 
