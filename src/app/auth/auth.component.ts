8ba9182c1376bd937f51936564584a7c26079695
Deepak Guptha <vzan2012@gmail.com>
2022-04-10 13:08:25 +0530

    fix: Updated Alert Component - close

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -86,7 +86,7 @@ export class AuthComponent implements OnInit, OnDestroy {
     const componentRef = hostViewContainerRef.createComponent(alertCmpFactory);
 
     componentRef.instance.message = message;
-    componentRef.instance.close.subscribe(() => {
+    this.closeSub = componentRef.instance.close.subscribe(() => {
       this.closeSub.unsubscribe();
       hostViewContainerRef.clear();
     });
