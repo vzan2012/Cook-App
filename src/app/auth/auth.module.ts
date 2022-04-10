c96b8eb4421e3a0db6199a74489b99d2fbae872b
Deepak Guptha <vzan2012@gmail.com>
2022-04-10 15:22:00 +0530

    feat: Updated LazyModules - Recipes Module

---
+++ src/app/auth/auth.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AuthComponent } from './auth.component';
+import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
+import { SharedModule } from '../shared/shared.module';
+
+const appRoutes: Routes = [{ path: 'auth', component: AuthComponent }];
+
+@NgModule({
+  declarations: [AuthComponent],
+  imports: [
+    CommonModule,
+    FormsModule,
+    SharedModule,
+    RouterModule.forChild(appRoutes),
+  ],
+})
+export class AuthModule {}
